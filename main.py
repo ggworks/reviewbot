79d41a70f2914c211b551e92b5d3986d49914a6e
hiitiger <lushang@outlook.com>
2023-05-19 16:42:49 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
 import hmac
 import hashlib
+import requests
+from openai import OpenAI, ChatCompletion
 
-import sys
 import os
 import logging
 import logging.config
@@ -12,12 +13,16 @@ logging.config.fileConfig(log_config, disable_existing_loggers=False)
 
 logger = logging.getLogger(__name__)
 
-sys.path.append('')
-
 app = FastAPI()
 
-SECRET_TOKEN = "helloworld"
 
+SECRET_TOKEN = "helloworld"
+OPENAI_API_KEY = os.getenv("API_KEY")
+GITLAB_API_TOKEN =  os.getenv("GITLAB_API_TOKEN")
+GITLAB_API_ORIGIN =  os.getenv("GITLAB_API_ORIGIN")
+
+
+openai = OpenAI(api_key=OPENAI_API_KEY)
 
 @app.post("/gitlab-webhook")
 async def gitlab_webhook(request: Request):
@@ -31,9 +36,36 @@ async def gitlab_webhook(request: Request):
 
     # Process webhook
     data = await request.json()
+    for commit in data.get('commits', []):
+        message = commit.get('message')
+        logger.info(f"Commit message: {message}")
 
-    logger.info(data)
-    # Do something with data...
+        # Send message to OpenAI for review
+        model = "gpt-3.5-turbo"  # You can use other models
+        messages = [{"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": f"Review this code commit message: {message}"}]
+        response = ChatCompletion.create(
+          model=model,
+          messages=messages
+        )
+
+        review = response['choices'][0]['message']['content']
+        logger.info(f"Review: {review}")
+
+        # Add review as a comment to the commit
+        project_id = data['project']['id']
+        commit_id = commit['id']
+        comment_url = f"{GITLAB_API_ORIGIN}/api/v4/projects/{project_id}/repository/commits/{commit_id}/comments"
+        comment_data = {
+            "note": review
+        }
+        headers = {
+            "PRIVATE-TOKEN": GITLAB_API_TOKEN
+        }
+        response = requests.post(comment_url, json=comment_data, headers=headers)
+
+        if response.status_code != 201:
+            logger.error(f"Failed to add comment to commit: {response.text}")
 
     # Return success message
     return {"message": "Webhook received and processed successfully"}
